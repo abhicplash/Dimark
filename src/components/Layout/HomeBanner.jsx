6811fdad1163f0fcfdecc1422b6d7f2cd2ecb92a
Abhilash CP <abhicplash@gmail.com>
2024-10-27 13:05:14 +0530

    home serviced on

--- src/components/Layout/HomeBanner.jsx
+++ src/components/Layout/HomeBanner.jsx
@@ -6,20 +6,27 @@ const HomeBanner = () => {
     <div className="HomeBanner-container">
       <h2 className="welcome">Welcome to NEW </h2>
       <span className="headbnr">
+        We believe in <br />
+        turning <span className="potential"> your </span>
+        <br />
+        vision <br />
+        into action
+      </span>
+      {/* <span className="headbnr">
         Unlock Your <br /> Digital
         <span className="potential"> Potential,</span>
         <br /> Achieve <br /> Excellence.
-      </span>
+      </span> */}
       <div className="bnrDetails">
-        <button class="learn-more">
-          <span aria-hidden="true" class="circle">
-            <span class="icon arrow" />
+        <button className="learn-more">
+          <span aria-hidden="true" className="circle">
+            <span className="icon arrow" />
           </span>
-          <span class="button-text">Learn More</span>
+          <span className="button-text">Learn More</span>
         </button>
         <div className="detailpara">
-          Navigating the Ever-Changing Online Terrain, Delivering Unparalleled
-          Success
+          we blend industry insights with creativity to craft solutions that
+          engage your audience and fuel growth.
         </div>
       </div>
     </div>
