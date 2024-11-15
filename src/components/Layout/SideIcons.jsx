f8517834dd77dfeb4f37fd9ee8186b26f4e6fae3
Abhilash CP <abhicplash@gmail.com>
2024-11-15 15:50:58 +0530

    whatsapp issue fixed

--- src/components/Layout/SideIcons.jsx
+++ src/components/Layout/SideIcons.jsx
@@ -1,66 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import "./SideIcons.css";
-// import { FaWhatsapp } from "react-icons/fa";
-// import { GiEnvelope } from "react-icons/gi";
-// import { RiPhoneFill } from "react-icons/ri";
-// import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
-
-// const SideIcons = () => {
-//   const [isVisible, setIsVisible] = useState(false);
-
-//   const toggleVisibility = () => {
-//     if (window.pageYOffset > 300) {
-//       setIsVisible(true);
-//     } else {
-//       setIsVisible(false);
-//     }
-//   };
-
-//   const scrollToTop = () => {
-//     window.scrollTo({
-//       top: 0,
-//       behavior: "smooth"
-//     });
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("scroll", toggleVisibility);
-//     return () => {
-//       window.removeEventListener("scroll", toggleVisibility);
-//     };
-//   }, []);
-//   return (
-//     <div className="icon-container">
-//       <div className="iconwrapper">
-//         <a
-//           href={`https://wa.me/+971542791548/?text=let me know more about the services provided by\n *Dimark Marketing Management* `}
-//           target="_blank"
-//           rel="noreferrer"
-//           title="whatsapp"
-//         >
-//           <FaWhatsapp />
-//         </a>
-//       </div>
-//       <div className="iconwrapper">
-//         <a href="mailto:info@dimark.ae" title="mail">
-//           <GiEnvelope />
-//         </a>
-//       </div>
-//       <div className="iconwrapper">
-//         <a href="tel:+971542791548" title="call">
-//           <RiPhoneFill />
-//         </a>
-//       </div>
-//       <div className="iconwrapper">
-//         {/* <a href=""> */}
-//         <MdOutlineKeyboardDoubleArrowUp onClick={scrollToTop} />
-//         {/* </a> */}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SideIcons;
 import React, { useEffect, useState } from "react";
 import "./SideIcons.css";
 import { FaWhatsapp } from "react-icons/fa";
@@ -98,7 +35,7 @@ const SideIcons = () => {
     <div className="icon-container">
       <div className="iconwrapper">
         <a
-          href="https://wa.me/+971542791548/?text=let me know more about the services provided by\n *Dimark Marketing Management*"
+          href="https://wa.me/+971542791548/?text=let me know more about the services provided by *Dimark Marketing Management*"
           target="_blank"
           rel="noreferrer"
           title="Whatsapp"
@@ -107,12 +44,12 @@ const SideIcons = () => {
         </a>
       </div>
       <div className="iconwrapper">
-        <a href="mailto:info@dimark.ae" target="_blank" title="Email">
+        <a href="mailto:info@dimark.ae" target="_blank" title="Email"   rel="noreferrer">
           <GiEnvelope />
         </a>
       </div>
       <div className="iconwrapper">
-        <a href="tel:+971542791548" target="_blank" title="Call">
+        <a href="tel:+971542791548" target="_blank" title="Call"   rel="noreferrer">
           <RiPhoneFill />
         </a>
       </div>
